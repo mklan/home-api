a95c5465b6d1bc04deea056c085cf7c87791aa53
Matthias Klan <matthias.klan@gmail.com>
2020-03-09 01:39:14 +0100

    Only log max 10 messages

--- src/api/logger/index.ts
+++ src/api/logger/index.ts
@@ -1,10 +1,13 @@
 const logs = [];
+const maxSize = 10;
 
 export const getLogs = (limit = 10) => (req, res) => {
   return res.send(logs);
 };
 
 export function addLog(req, res) {
+  console.log({ time: new Date(), payload: req.body.payload });
+  if (logs.length >= maxSize) logs.shift();
   logs.push({ time: new Date(), payload: req.body.payload });
   return res.send(logs);
 }
